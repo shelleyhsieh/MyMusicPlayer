6addbd4fb2d0607af1575bbdc4be634f99cd8d49
Shelley Hsieh <shuan0720@gmail.com>
2023-01-12 16:17:52 +0800

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by shelley on 2023/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
